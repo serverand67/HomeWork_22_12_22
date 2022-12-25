be287654e4257b0af0eba3b31ecd89b78f66e50a
unknown <veronika040182@hotmail.com>
2022-12-25 15:58:08 +0300

    задача 23

---
+++ task23/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int getUserValues(string massage)     //Функция ввода данных
+{
+    Console.WriteLine(massage);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+void Cube(int number)     // функция вычисления куба чисел от 1 до N
+{
+    for (int i = 1; i <= number; i++)
+    { 
+       int cube =  i * i * i;
+       Console.Write($"{cube} ");
+    }
+}
+int N = getUserValues("Введите число N ");
+Cube(N);
